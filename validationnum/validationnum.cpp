d14d38278a966863cb645bde02a7103e5bafb302
Eloquade <perez.lawrence08@gmail.com>
2021-02-22 21:18:34 +0800

    New validations

---
+++ validationnum/validationnum.cpp
@@ -0,0 +1,30 @@
+// validationnum.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int num,dnum; int done=0; //0 is false, 1 is true
+	do{
+		try{
+          cout<<"Enter a number ";
+		  cin>>num;
+		  if(!cin || (num<50)|| (num>100))
+			  throw dnum;
+             done=1;   //done becomes true
+		}
+		   catch (int d)
+		   {
+              cout<<"Number only and must be between 50 to 100!!! "<<endl;
+			  cin.clear();
+			  cin.ignore(100,'\n');
+		   }
+	}while(!done);
+	cout<<"The number entered is "<<num<<endl;
+system("pause");
+}
